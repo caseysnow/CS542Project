a84881f5f700ccf3f528d19171f609e252d954a9
caseysnow <cesnow2@wpi.edu>
2022-12-06 14:22:00 -0500

    log in

--- server/routes/index.js
+++ server/routes/index.js
@@ -20,6 +20,10 @@ router.get('/product', async (req, res, next) => {
     res.sendFile('server/views/product.html', {root: '.'});
 });
 
+router.get('/newUser', async (req, res, next) => {
+    res.sendFile('server/views/newUser.html', {root: '.'});
+});
+
 router.get('/product/:product_id', async (req, res, next) => {
     let results;
     try{
@@ -62,6 +66,19 @@ router.get('/searchRes', async (req, res, next) => {
     }
 });
 
+router.get('/newCart/:username', async (req, res, next) => {
+    
+    // console.log(req.query.searchInput);
+    let results;
+    try{
+        results = await db.createCart(req.params.username);
+        res.json(results);
+    }catch(e){
+        console.log(e);
+        res.sendStatus(500);
+    }
+});
+
 router.get('/loggedIn', async (req, res, next) => {
     res.sendFile('server/views/loggedIn.html', {root: '.'});
 });
@@ -70,23 +87,37 @@ router.get('/pleaseWork', async (req, res, next) => {
     
     // console.log(req.query.uname);
     // console.log(req.query.psw);
+    // console.log(req.query.admin);
+    // console.log(req.query.addr);
+
     let results;
+
     try{
         results = await db.getUsername(req.query.uname);
+        console.log(results);
         // res.json(results);
         try{
-            results = await db.login(req.query.uname, req.query.psw);
-            res.json(results);
+            if(req.query.addr == null){
+                results = await db.login(req.query.uname, req.query.psw);
+                res.json(results);
+            }
+            else{
+                results = await db.addUser(req.query.uname, req.query.psw, req.query.admin, req.query.addr);
+                res.json(results);
+                console.log(results);
+            }
             // console.log(results);
             // user.displayUser(results);
         }catch(e){
-            console.log(e);
-            //should make this password doesn't match
+            console.log(e.sqlMessage);
+            res.send(e);
+
         }
     }catch(e){
         console.log(e);
         //should make this cannot find username
     }
 });
+
 
 module.exports = router; 
--- server/views/cart.js
+++ server/views/cart.js
@@ -1,33 +1,43 @@
 $(document).ready(function () {
     // tryingToReach();
-    const auth = new Auth();
+    // const auth = new Auth();
+    const username = localStorage.getItem("username");
+    console.log(username);
 
-document.querySelector(".logout").addEventListener("click", (e) => {
-    auth.logOut();
-});
+// document.querySelector(".logout").addEventListener("click", (e) => {
+//     auth.logOut();
+// });
     $.ajax({
         method:'GET',
-        url:'http://127.0.0.1:3000/cart/' + user.username,
+        url:'http://127.0.0.1:3000/cart/' + username,
         success:function(response){
             console.log(response);
-            // integrate(response);
+            integrate(response);
+            var element = document.getElementById('cartHeader');
+            element.innerHTML = username + '\'s cart';
+        },
+        error: function(textStatus, errorThrown) { 
+            alert("There are no items in your cart"); 
+            var element = document.getElementById('cartHeader');
+            element.innerHTML = username + '\'s cart';
         }
+        
     });
-
-    // async function isLoggedIn () {
-    //     const token = store.get('token')
-    //     if (!token) return false
-    //   }
-
-    //   async function autoRedirect () {
-    //     const validLogin = await isLoggedIn()
-    //     if (!validLogin && location.pathname !== '/login/') redirect('/login')
-    //     if (validLogin && location.pathname === '/login/') redirect('/')
-    //   }
     
     function integrate(data){
-        var element = document.getElementById('cartData');
         console.log(data);
-        element.innerHTML = data.username + '\'s cart';
+        var table = document.getElementById('cartTable');
+        // for(i = 0; i < data.length; i ++){
+            var row = table.insertRow(1);
+            var cell1 = row.insertCell(0);
+            var cell2 = row.insertCell(1);
+            var cell3 = row.insertCell(2);
+            // cell1.innerHTML = data[i].username;
+            // cell2.innerHTML = data[i].quantity;
+            // cell3.innerHTML = data[i].price;
+            cell1.innerHTML = data.item_id;
+            cell2.innerHTML = data.quantity;
+            cell3.innerHTML = data.price;
+        // }
     }
   });
